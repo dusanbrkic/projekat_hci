83ed5646e03b364c8912ff3645e4182b290bedb1
Filip <filip.zivanac@gmail.com>
2021-06-06 00:56:12 +0200

    Pregled saradnika

---
+++ PROJEKAT_HCI/PROJEKAT_HCI/View/OrganizatorSaradnikWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using PROJEKAT_HCI.Model;
+using PROJEKAT_HCI.View;
+
+namespace PROJEKAT_HCI.View
+{
+    /// <summary>
+    /// Interaction logic for OrganizatorSaradnikWindow.xaml
+    /// </summary>
+    public partial class OrganizatorSaradnikWindow : Window
+    {
+        public Saradnik Saradnik { get; set; }
+        public OrganizatorSaradnici os { get; set; }
+        public OrganizatorSaradnikWindow(Saradnik s, OrganizatorSaradnici ost)
+        {
+            this.Saradnik = s;
+            this.os = ost;
+            InitializeComponent();
+            this.Title = Saradnik.Naziv;
+            Opis.Content = Saradnik.Opis;
+            Lokacija.Content = Saradnik.Lokacija;
+        }
+
+        private void Nazad_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            os.Show();
+        }
+        private void NovaPonuda_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
