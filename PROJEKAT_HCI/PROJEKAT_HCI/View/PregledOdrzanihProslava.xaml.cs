2b894507cd1a15904f4bc217325462441de6d86a
Nikola Petrovic <s.nino.petrovic@gmail.com>
2021-06-09 23:56:52 +0200

    Doradjeni prozori i dugmici popravljeni

---
+++ PROJEKAT_HCI/PROJEKAT_HCI/View/PregledOdrzanihProslava.xaml.cs
@@ -0,0 +1,138 @@
+﻿using MaterialDesignThemes.Wpf;
+using PROJEKAT_HCI.Database;
+using PROJEKAT_HCI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJEKAT_HCI.View
+{
+    /// <summary>
+    /// Interaction logic for PregledOdrzanihProslava.xaml
+    /// </summary>
+    public partial class PregledOdrzanihProslava : Window
+    {
+        public class Dugme : Button
+        {
+            public Proslava Proslava { get; set; }
+        }
+        private Klijent klijent { get; set; }
+        public PregledOdrzanihProslava(Klijent k)
+        {
+            this.klijent = k;
+            InitializeComponent();
+            InitializeComponent();
+            this.klijent = k;
+            using (var db = new ProjectDatabase())
+            {
+                //var proslave = (from p in db.Proslave where p.Klijent.Id == klijent.Id select p);
+                foreach (Proslava p in (from p in db.Proslave where p.Klijent.Id == klijent.Id && p.StatusProslave == StatusProslave.ORGANIZOVANO select p).ToList())
+                {
+                    int suma = 0;
+                    ;
+                    foreach (Zadatak z in db.Proslave.Find(p.Id).PredlogProslave.Zadaci)
+                    {
+                        suma += z.Ponuda.Cena;
+                    }
+                    Card card = new Card();
+                    card.Width = 220;
+                    card.Height = 220;
+                    card.Margin = new Thickness(5, 5, 5, 5);
+                    TextBox tb = new TextBox()
+                    {
+                        IsReadOnly = true,
+                        TextWrapping = TextWrapping.Wrap,
+                        Text = p.Naziv + "\n Cena: " + suma,
+                        Width = 200,
+                        Height = 100,
+                        Margin = new Thickness(5, 5, 5, 5),
+                        Foreground = new SolidColorBrush(Colors.Black),
+                        TextAlignment = TextAlignment.Center,
+                        FontSize = 20,
+                        VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        AcceptsReturn = true
+                    };
+                    StackPanel sp1 = new StackPanel() { Orientation = Orientation.Vertical };
+                    sp1.Margin = new Thickness(0, 0, 0, 10);
+                    sp1.Children.Add(tb);
+                    Grid g = new Grid();
+                    g.Children.Add(sp1);
+
+                    card.Content = g;
+                    wrapper.Children.Add(card);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            KlijentWindow kw = new KlijentWindow(this.klijent);
+            kw.Show();
+            this.Hide();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            wrapper.Children.Clear();
+            using (var db = new ProjectDatabase())
+            {
+                foreach (Proslava p in (from p in db.Proslave where p.Klijent.Id == klijent.Id && p.Naziv.Contains(search.Text) && p.StatusProslave == StatusProslave.ORGANIZOVANO select p).ToList())
+                {
+
+                    int suma = 0;
+                    ;
+                    foreach (Zadatak z in db.Proslave.Find(p.Id).PredlogProslave.Zadaci)
+                    {
+                        suma += z.Ponuda.Cena;
+                    }
+                    Card card = new Card();
+                    card.Width = 220;
+                    card.Height = 220;
+                    card.Margin = new Thickness(5, 5, 5, 5);
+                    TextBox tb = new TextBox()
+                    {
+                        IsReadOnly = true,
+                        TextWrapping = TextWrapping.Wrap,
+                        Text = p.Naziv + "\n Cena: " + suma,
+                        Width = 200,
+                        Height = 100,
+                        Margin = new Thickness(5, 5, 5, 5),
+                        Foreground = new SolidColorBrush(Colors.Black),
+                        TextAlignment = TextAlignment.Center,
+                        FontSize = 20,
+                        VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        AcceptsReturn = true
+                    };
+                    StackPanel sp1 = new StackPanel() { Orientation = Orientation.Vertical };
+                    sp1.Margin = new Thickness(0, 0, 0, 10);
+                    sp1.Children.Add(tb);
+                    Grid g = new Grid();
+                    g.Children.Add(sp1);
+
+                    card.Content = g;
+                    wrapper.Children.Add(card);
+                } 
+            } 
+        }
+
+
+        private void UrediProfilBtn_Click(object sender, RoutedEventArgs e)
+        {
+            EditProfile ep = new EditProfile(this, klijent);
+            ep.Show();
+            this.Hide();
+        }
+    }
+}
--- PROJEKAT_HCI/PROJEKAT_HCI/View/RasporedSedenja.xaml.cs
+++ PROJEKAT_HCI/PROJEKAT_HCI/View/RasporedSedenja.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using PROJEKAT_HCI.Model;
+using PROJEKAT_HCI.Database;
+using System.ComponentModel;
 namespace PROJEKAT_HCI.View
 {
     /// <summary>
@@ -25,12 +28,27 @@ namespace PROJEKAT_HCI.View
             InitializeComponent();
         }
 
-        class ExampleItemViewModel
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            public bool CanAcceptChildren { get; set; }
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "CSV Files(*.CSV;)|*.CSV;|All files (*.*)|*.*";
+            //bool? okdia = ofd.ShowDialog();
+            string sFileName = ofd.FileName;         
+            using (var reader = new StreamReader(sFileName))
+            {
+                List<String> gosti = new List<String>();
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    String[] temp = line.Split(',');
 
-            public int size { get; set; }
-            public ObservableCollection<ExampleItemViewModel> Children { get; private set; }
+                    gosti.Add(temp[0] + " " + temp[1]);
+                }
+            }
+            //Nullable<bool> okdia = ofd.ShowDialog();
         }
     }
 }
