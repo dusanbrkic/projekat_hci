6338b5c7845230f16073060597cbff3c1d28a649
nemanjamt <65918711+nemanjamt@users.noreply.github.com>
2021-06-09 16:12:50 +0200

    Dodavanje prozora za ponude

---
+++ PROJEKAT_HCI/PROJEKAT_HCI/View/AzuriranjePonudeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PROJEKAT_HCI.View
+{
+    /// <summary>
+    /// Interaction logic for AzuriranjePonudeWindow.xaml
+    /// </summary>
+    public partial class AzuriranjePonudeWindow : Window
+    {
+        public AzuriranjePonudeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
